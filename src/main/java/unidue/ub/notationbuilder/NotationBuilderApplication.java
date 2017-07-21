165c2125c2d163b7432e687c540df2617acd0bd0
Eike Spielberg <Eike.Spielberg@uni-due.de>
2017-07-21 14:10:31 +0200

    initial commit

---
+++ src/main/java/unidue/ub/notationbuilder/NotationBuilderApplication.java
@@ -0,0 +1,12 @@
+package unidue.ub.notationbuilder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotationBuilderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotationBuilderApplication.class, args);
+	}
+}
